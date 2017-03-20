d7f603a9cc228f07565a96da183bfab511acf0f9
Mark Junker <fubar-coder@live.de>
2017-03-20 22:11:34 +0100

    Added options

---
+++ FubarDev.WebDavServer.Locking.TextFile/TextFileLockManagerOptions.cs
@@ -0,0 +1,9 @@
+﻿namespace FubarDev.WebDavServer.Locking.TextFile
+{
+    public class TextFileLockManagerOptions : ILockManagerOptions
+    {
+        public string LockFileName { get; set; }
+
+        public ILockTimeRounding Rounding { get; set; } = new DefaultLockTimeRounding(DefaultLockTimeRoundingMode.OneSecond);
+    }
+}
