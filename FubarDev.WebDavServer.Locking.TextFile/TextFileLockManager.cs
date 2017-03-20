daee6b1d73560f102d3c8a3abff76cac1c275af3
Mark Junker <fubar-coder@live.de>
2017-03-20 21:54:02 +0100

    Projektdateien hinzufügen.

---
+++ FubarDev.WebDavServer.Locking.TextFile/TextFileLockManager.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+using Microsoft.Extensions.Logging;
+
+namespace FubarDev.WebDavServer.Locking.TextFile
+{
+    public class TextFileLockManager : LockManagerBase
+    {
+        public TextFileLockManager(ILockCleanupTask cleanupTask, ISystemClock systemClock, ILogger logger, ILockManagerOptions options = null)
+            : base(cleanupTask, systemClock, logger, options)
+        {
+        }
+
+        protected override Task<ILockManagerTransaction> BeginTransactionAsync(CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
