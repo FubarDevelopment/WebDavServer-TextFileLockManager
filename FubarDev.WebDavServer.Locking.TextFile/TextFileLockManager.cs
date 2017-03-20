d5fa8146b74c47b7a00e2fabec106603f1152def
Mark Junker <fubar-coder@live.de>
2017-03-20 22:13:43 +0100

    Use options and pass it to the base class

--- FubarDev.WebDavServer.Locking.TextFile/TextFileLockManager.cs
+++ FubarDev.WebDavServer.Locking.TextFile/TextFileLockManager.cs
@@ -3,13 +3,14 @@ using System.Threading;
 using System.Threading.Tasks;
 
 using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
 
 namespace FubarDev.WebDavServer.Locking.TextFile
 {
     public class TextFileLockManager : LockManagerBase
     {
-        public TextFileLockManager(ILockCleanupTask cleanupTask, ISystemClock systemClock, ILogger logger, ILockManagerOptions options = null)
-            : base(cleanupTask, systemClock, logger, options)
+        public TextFileLockManager(ILockCleanupTask cleanupTask, ISystemClock systemClock, ILogger logger, IOptions<TextFileLockManagerOptions> options)
+            : base(cleanupTask, systemClock, logger, options.Value)
         {
         }
 
