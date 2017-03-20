e86a131b02b2fbe00d5114bef542295e31d7c286
Mark Junker <fubar-coder@live.de>
2017-03-20 22:39:43 +0100

    The class used to store the active locks

---
+++ FubarDev.WebDavServer.Locking.TextFile/ActiveLock.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Xml.Linq;
+
+namespace FubarDev.WebDavServer.Locking.TextFile
+{
+    public class ActiveLock : IActiveLock
+    {
+        public string Path { get; set; }
+
+        public string Href { get; set; }
+
+        public bool Recursive { get; set; }
+
+        public string AccessType { get; set; }
+
+        public string ShareMode { get; set; }
+
+        public TimeSpan Timeout { get; set; }
+
+        public string StateToken { get; set; }
+
+        public DateTime Issued { get; set; }
+
+        public DateTime? LastRefresh { get; set; }
+
+        public DateTime Expiration { get; set; }
+
+        public XElement Owner { get; set; }
+
+        public XElement GetOwner()
+        {
+            return Owner;
+        }
+    }
+}
